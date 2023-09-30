57e9076974812e086a6624236329dfeceb748596
imgon <kosedy@ualberta.ca>
2023-09-30 11:31:29 -0600

    Finished

--- app/src/main/java/com/example/expbook/MainActivity.java
+++ app/src/main/java/com/example/expbook/MainActivity.java
@@ -28,17 +28,8 @@ public class MainActivity extends AppCompatActivity implements AddExpenseFragmen
     ListView expenseList;
     ArrayAdapter<Expense> expenseAdapter;
     ArrayList<Expense> dataList;
-    EditText form_expense_name;
     private Expense selectedExpense;
     private TextView total_expense;
-
-    Expense edited_expense = null;
-    SimpleDateFormat formatter;
-    private static final int ADD_EXPENSE_REQUEST_CODE = 1;
-    private static final int EDIT_EXPENSE_REQUEST_CODE = 2;
-    private static final int DEL_EXPENSE_REQUEST_CODE = 3;
-
-    Expense selectedFromList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +38,10 @@ public class MainActivity extends AppCompatActivity implements AddExpenseFragmen
 
         BigDecimal charge = BigDecimal.valueOf(123.00);
 
-//        Expense exp1 = new Expense("Expense 1", "2023","8", charge, "comment");
-//        Expense []expenses = {exp1};
         total_expense = findViewById(R.id.total_expense_val);
 
         dataList = new ArrayList<Expense>();
-//        dataList.add(exp1);
+
         Date date = new Date();
         SimpleDateFormat f = new SimpleDateFormat("yyyy-MM");
 
@@ -76,97 +65,6 @@ public class MainActivity extends AppCompatActivity implements AddExpenseFragmen
         addButton.setOnClickListener(v -> {
             new AddExpenseFragment().show(getSupportFragmentManager(), "ADD_CITY");
         });
-//
-//        FloatingActionButton go_to_add_expense = findViewById(R.id.go_to_add_expense);
-//        go_to_add_expense.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, ExpenseActivity.class);
-//                startActivityForResult(intent, ADD_EXPENSE_REQUEST_CODE);
-//            }
-//        });
-//
-//        expenseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Expense expense = (Expense) expenseList.getItemAtPosition(position);
-//
-//                Intent intent = new Intent(MainActivity.this, ExpenseActivity.class);
-//                intent.putExtra("Edit_Expense", expense);
-//                intent.putExtra("position", position);
-//                startActivityForResult(intent, EDIT_EXPENSE_REQUEST_CODE);
-//            }
-//        });
-//
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//            if(ADD_EXPENSE_REQUEST_CODE == requestCode) {
-//                if (resultCode == RESULT_OK) {
-//
-//                    String expense_name = data.getStringExtra("Expense_name");
-//                    int expense_charge = Integer.parseInt(data.getStringExtra("Expense_charge"));
-//                    String expense_comment = data.getStringExtra("Expense_comment");
-//                    String expense_date_year = data.getStringExtra("Expense_date_year");
-//                    String expense_date_month = data.getStringExtra("Expense_date_month");
-//
-//
-//                    SimpleDateFormat f = new SimpleDateFormat("yyyy-MM");
-//                    String formattedDate = expense_date_year + "-" + expense_date_month;
-//
-//                    Expense added_expense = new Expense(expense_name, formattedDate, expense_charge, expense_comment);
-//
-//                    // Add the user input to your dataList (expense list)
-//                    dataList.add(added_expense);
-//
-//                    // Notify the adapter that the data set has changed
-//                    expenseAdapter.notifyDataSetChanged();
-//                }
-//            } else if(EDIT_EXPENSE_REQUEST_CODE == requestCode) {
-//                if (resultCode == RESULT_OK) {
-//                    if(data.hasExtra("isDeleted")){
-//                        int position = data.getIntExtra("Position", -1);
-//                        if (position != -1) {
-//                            // Update the expenseList and refresh the ListView
-//                            dataList.remove(position);
-//                            expenseAdapter.notifyDataSetChanged();  // Update the ListView
-//                        }
-//                    } else {
-//
-//                        String expense_name = data.getStringExtra("Expense_name");
-//                        int edit_expense_charge = Integer.parseInt(data.getStringExtra("Expense_charge"));
-//                        String expense_comment = data.getStringExtra("Expense_comment");
-//                        String expense_date_year = data.getStringExtra("Expense_date_year");
-//                        String expense_date_month = data.getStringExtra("Expense_date_month");
-//
-//
-//                        int position = data.getIntExtra("Position", -1);
-//
-//                        String formattedDate = expense_date_year + "-" + expense_date_month;
-//                        Expense edited_expense = new Expense(expense_name, formattedDate, edit_expense_charge, expense_comment);
-//
-//
-//                        if (position != -1) {
-//                            // Update the expenseList and refresh the ListView
-//                            dataList.set(position, edited_expense);
-//                            expenseAdapter.notifyDataSetChanged();  // Update the ListView
-//                        }
-//                    }
-//
-//                }
-//            } else if (DEL_EXPENSE_REQUEST_CODE == requestCode) {
-//                if (resultCode == RESULT_OK) {
-//                    int position = data.getIntExtra("Position", -1);
-//                    if (position != -1) {
-//                        // Update the expenseList and refresh the ListView
-//                        dataList.remove(position);
-//                        expenseAdapter.notifyDataSetChanged();  // Update the ListView
-//                    }
-//
-//                }
-//            }
 
     }
 
